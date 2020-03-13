1ae4feef90fef7d6354c91d1510c2bc0d690cf48
Ashfaaq Mohammed <iim.ashfaaq@gmail.com>
2020-03-13 09:48:00 +0530

    Resume data added

---
+++ src/resumeData.js
@@ -0,0 +1,100 @@
+let resumeData = {
+    "imagebaseurl":"https://iimashfaaq.github.io/",
+    "name": "Ashfaaq IIM",
+    "role": "Frontend Developer and Data Scientist",
+    "linkedinId":"Your LinkedIn Id",
+    "skypeid": "Your skypeid",
+    "roleDescription": " I like dabbling in various parts of software development and like to learn about trending technologies, write technical articles or surf for anything interesting in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/ashfaaq-i-i-m-b40a60136/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/iimashfaaq",
+          "className":"fa fa-github"
+        }
+      // ,
+      //   {
+      //     "name":"skype",
+      //     "url":"http://twitter.com/",
+      //     "className":"fa fa-twitter"
+      //   }
+      ],
+    "aboutme":"I am currently a 3rd year student at PSG College of Technology and am pursuing M.Sc (Software Systems). I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"Coimbatore, Tamil Nadu, India",
+    "website":"https://iimashfaaq.github.io",
+    "education":[
+      {
+        "UniversityName":"PSG College of Technology",
+        "specialization":"MSc - Software Systems",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2022",
+        "Achievements":""
+      }
+    ],
+    "projects":[
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
