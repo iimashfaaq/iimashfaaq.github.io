cdcaed37e9954f7149b37595eb3fde802b4ec192
Ashfaaq Mohammed <iim.ashfaaq@gmail.com>
2020-03-19 11:22:22 +0530

    Update

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/iimashfaaq.github.io/precache-manifest.5533eaebc9dc7e27431ffcbabc281caa.js"
+  "/iimashfaaq/precache-manifest.ee4dfc2ced844f62bbeb7489cedb007d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/iimashfaaq.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/iimashfaaq/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
