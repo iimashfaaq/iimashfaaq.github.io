e34e597c58060a870a2e3b533650ad2a0e2c5b24
Ashfaaq I I M <iim.ashfaaq@gmail.com>
2020-03-19 10:30:51 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,20 +7,27 @@ import Testimonials from  './components/Testimonials';
 import ContactUs from './components/ContactUs';
 import Footer from './components/Footer';
 import resumeData from './resumeData';
+
+function initializeReactGA() {
+ 	ReactGA.initialize('UA-146779656-1');
+	ReactGA.pageview('/homepage');
+}
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Header resumeData={resumeData}/>
-        <About resumeData={resumeData}/>
-        <Resume resumeData={resumeData}/>
-        <Portfolio resumeData={resumeData}/>
-        <Testimonials resumeData={resumeData}/>
-        <ContactUs resumeData={resumeData}/>
-        <Footer resumeData={resumeData}/>
-      </div>
-    );
-  }
+	  initializeReactGA();
+	  render() {
+			return (
+				  <div className="App">
+						<Header resumeData={resumeData}/>
+						<About resumeData={resumeData}/>
+						<Resume resumeData={resumeData}/>
+						<Portfolio resumeData={resumeData}/>
+						<Testimonials resumeData={resumeData}/>
+						<ContactUs resumeData={resumeData}/>
+						<Footer resumeData={resumeData}/>
+				  </div>
+			);
+  		}
 }
 
 export default App;
