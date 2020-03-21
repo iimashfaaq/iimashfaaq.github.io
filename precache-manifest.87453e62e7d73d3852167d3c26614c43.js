3d7894a73486cb1edd34a85c1f372aa6e4028b74
Ashfaaq Mohammed <iim.ashfaaq@gmail.com>
2020-03-21 09:08:55 +0530

    Updates

--- precache-manifest.87453e62e7d73d3852167d3c26614c43.js
+++ precache-manifest.87453e62e7d73d3852167d3c26614c43.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "acc8d4edbf9e37ba40190f475b802e99",
+    "revision": "57f262e7096f5bfa15bddcc28326d087",
     "url": "/iimashfaaq/index.html"
   },
   {
-    "revision": "ca48bca2fc5b4165d15a",
+    "revision": "26bbf112060c11f2f748",
     "url": "/iimashfaaq/static/css/main.e8c24495.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/iimashfaaq/static/js/2.3fd20b11.chunk.js"
   },
   {
-    "revision": "ca48bca2fc5b4165d15a",
-    "url": "/iimashfaaq/static/js/main.f1cb536b.chunk.js"
+    "revision": "26bbf112060c11f2f748",
+    "url": "/iimashfaaq/static/js/main.e4b3faf1.chunk.js"
   },
   {
     "revision": "a06bc3d702db08a65697",
