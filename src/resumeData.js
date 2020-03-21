f8899c221c02908f99047d0f929db3a7ba590e8b
Ashfaaq Mohammed <iim.ashfaaq@gmail.com>
2020-03-21 09:08:18 +0530

    A few updates

--- src/resumeData.js
+++ src/resumeData.js
@@ -2,9 +2,8 @@ let resumeData = {
     "imagebaseurl":"https://iimashfaaq.github.io/",
     "name": "Ashfaaq IIM",
     "role": "CS Student | Autodidact",
-    "linkedinId":"Your LinkedIn Id",
-    "skypeid": "Your skypeid",
-    "roleDescription": "I have chosen an industry where I can help improve the world for our future generation and make ours a little bit more fun. Fluent with programming languages such as C++, C#, Python, ReactJs and Java. Comfortable with web development.",
+    "linkedinId":"",
+    "roleDescription": "Self-driven and motivated Software student adept at developing and implementing new software platforms. Possessing great analytical skills, ability to work in team environments and strong attention to detail.",
     "socialLinks":[
         {
           "name":"linkedin",
@@ -17,7 +16,7 @@ let resumeData = {
           "className":"fa fa-github"
         }
       ],
-    "aboutme":"I am currently a 3rd year student at PSG College of Technology and pursuing my Masters in Software systems. I am a self taught Software Developer. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "aboutme":"I am currently a 3rd year student at PSG College of Technology and pursuing my Masters in Software systems. I have chosen an industry where I can help improve the world for our future generation and make ours a little bit more fun. Fluent with programming languages such as C++, C#, Python, ReactJs and Java. Comfortable with web development.",
     "address":"India",
     "website":"https://iimashfaaq.github.io",
     "education":[
@@ -51,36 +50,26 @@ let resumeData = {
     ],
     "portfolio":[
       {
-        "name":"project1",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
+        "name":"HACK[IN]",
+        "description":"An international level symposium event created on ReactJs for the UI and NodeJs as backend API calls. Players data was stored using MongoDB hosted with the help of aws servers. All commits were made on GitLab platform.",
+        "imgurl":"../public/images/portfolio/console.jpg"
       },
       {
-        "name":"project2",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/project.jpg"
+        "name":"P2P Group Chat",
+        "description":"A python based group chat that implements a centralized  group chat system. The chat system, designed using the Tkinter package, allows chat between peers through a central host.",
+        "imgurl": "../public/images/portfolio/coffee.jpg"
       },
       {
-        "name":"project3",
-        "description":"mobileapp",  
+        "name":"WannaFry",
+        "description":"An implementation of a Ransomware developed in Python with an intention to understand how a Ransomware attack is deployed. This ransomware encrypts the target directories entirely and the encryption of files is done with the PyCrypto module.",  
         "imgurl":"images/portfolio/project2.png"
       },
       {
-        "name":"project4",
-        "description":"mobileapp",
+        "name":"RC-Boat",
+        "description":"a remote controlled boat compatible with any bluetooth enabled device. This DIY project used an Arduino board as its heart and a motor driver which controlled the back propellers of the boat.",
         "imgurl":"images/portfolio/phone.jpg"
       }
-    ],
-    "testimonials":[
-      {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
-      },
-      {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
-      }
     ]
-  }
+}
   
   export default resumeData
