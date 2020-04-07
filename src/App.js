e466f02ae8672bb7b037d6ba3000c1319273585a
Ashfaaq Mohammed <iim.ashfaaq@gmail.com>
2020-04-07 11:39:39 +0530

    Added google analytics

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import ReactGA from 'react-ga';
+
 import Header from './components/Header';
 import About from './components/About';
 import Resume from './components/Resume';
@@ -7,8 +9,15 @@ import ContactUs from './components/ContactUs';
 import Footer from './components/Footer';
 import resumeData from './resumeData';
 
+
+function initializeReactGA() {
+    ReactGA.initialize('UA-159216199-2');
+    ReactGA.pageview('/');
+}
+
 class App extends Component {
 	render() {
+		initializeReactGA();
 		return (
 				<div className="App">
 					<Header resumeData={resumeData}/>
